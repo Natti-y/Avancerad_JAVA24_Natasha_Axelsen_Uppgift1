a3c0045d0c4921da08d5d408bb1f978e4d9a01f2
Natti-y <nyxx769@gmail.com>
2024-12-09 21:44:35 +0100

    Delete Avancerad_JAVA24_Natasha_Axelsen_Uppgift1 directory

--- Avancerad_JAVA24_Natasha_Axelsen_Uppgift1/src/main/java/Uppgift1/Main.java
+++
@@ -1,7 +0,0 @@
-package Uppgift1;
-
-public class Main {
-    public static void main(String[] args) {
-        RentalServices.main(args);
-    }
-}
--- Avancerad_JAVA24_Natasha_Axelsen_Uppgift1/src/main/java/Uppgift1/Rentable.java
+++
@@ -1,8 +0,0 @@
-package Uppgift1;
-
-public interface Rentable {
-    void rentVehicle(int days);
-    void returnVehicle();
-    double calculateRentalCost(int days);
-}
-
